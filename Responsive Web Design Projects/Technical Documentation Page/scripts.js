c86832e41cd75997d04a8e0ed8561b3a56f8a3c9
premkiran7 <premkiran7@gmail.com>
2020-09-18 22:29:37 +0530

    [technical documentation page] removed console.log

--- Responsive Web Design Projects/Technical Documentation Page/scripts.js
+++ Responsive Web Design Projects/Technical Documentation Page/scripts.js
@@ -2,7 +2,6 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-header");
 
 accordionItemHeaders.forEach(function (accordionItemHeader) {
   accordionItemHeader.addEventListener("click", function (event) {
-    console.log(accordionItemHeader);
     accordionItemHeader.classList.toggle("active");
   });
 });
