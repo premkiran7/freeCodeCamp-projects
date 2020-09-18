fe74a2773adde812aed4d718eb2f95fac72a0960
premkiran7 <premkiran7@gmail.com>
2020-09-18 20:49:36 +0530

    [technical documentation page] added content sections and media queries

--- Responsive Web Design Projects/Technical Documentation Page/scripts.js
+++ Responsive Web Design Projects/Technical Documentation Page/scripts.js
@@ -1,7 +1,5 @@
 const accordionItemHeaders = document.querySelectorAll(".accordion-header");
 
-console.log(accordionItemHeaders, "asd");
-
 accordionItemHeaders.forEach(function (accordionItemHeader) {
   accordionItemHeader.addEventListener("click", function (event) {
     console.log(accordionItemHeader);
