685dacb7e414459468c2d014b8698c889c3d474c
premkiran7 <premkiran7@gmail.com>
2020-09-28 21:57:56 +0530

    [roman-numeral-converter] added solution for roman numeral converter project

--- JS Algo & DS Projects/Roman Numeral Converter/solution.js
+++ JS Algo & DS Projects/Roman Numeral Converter/solution.js
@@ -1,14 +1,32 @@
+// no single roman numeral letter should get repeated more
+// than thrice, thus added prebuilt numerals for 4, 9 etc
+const romanNumerals = {
+  1: "I",
+  4: "IV",
+  5: "V",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+};
+
 function convertToRoman(num) {
-  const romanNumerals = {
-    1: "I",
-    5: "V",
-    10: "X",
-    50: "L",
-    100: "C",
-    500: "D",
-    1000: "M",
-  };
-  return num;
+  let convertedNum = "";
+  for (let [constant, romanNumeral] of Object.entries(
+    romanNumerals
+  ).reverse()) {
+    while (num >= constant) {
+      convertedNum += romanNumeral;
+      num = num - constant;
+    }
+  }
+  return convertedNum;
 }
 
-convertToRoman(36);
+console.log(convertToRoman(2));
